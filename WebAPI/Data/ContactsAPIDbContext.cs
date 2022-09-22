66bc3ac8c301cc04d4030f4e6ca4c7124ea414e2
MAFA448 <mafasmohamed48@gmail.com>
2022-09-22 22:20:04 +0530

    Add project files.

---
+++ WebAPI/Data/ContactsAPIDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPI.Models;
+
+namespace WebAPI.Data
+{
+    public class ContactsAPIDbContext : DbContext
+    {
+        public ContactsAPIDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
